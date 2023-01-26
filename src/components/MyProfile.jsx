651c0bca325b47d48019afcc0646f1f06a723fea
Milos Andric <milosandric2000@gmail.com>
2023-01-26 18:43:13 +0100

    Zavrseno prikazivanje informacija o korisniku

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -4,7 +4,8 @@ import { useNavigate } from 'react-router-dom';
 import Navbar from "./Navbar";
 
 export default function MyProfile() {
-  var data;
+  var myError = JSON.parse("{\"id\":-1,\"email\":\"Error...\",\"ime\":\"Error...\",\"prezime\":\"Error...\",\"username\":\"Error...\",\"datum_rodjenja\":\"Error...\",\"datum_pravljenja_naloga\":\"Error...\",\"aktiviran\":\"true\"}")
+  const [podaci, setPodaci] = useState(JSON.parse("{\"id\":-1,\"email\":\"Loading...\",\"ime\":\"Loading...\",\"prezime\":\"Loading...\",\"username\":\"Loading...\",\"datum_rodjenja\":\"Loading...\",\"datum_pravljenja_naloga\":\"Loading...\",\"aktiviran\":\"true\"}"));
   useEffect(() => {
     fetch('http://localhost:8080/api/user/ulogovani', { mode: 'cors', credentials: 'include' })
       .then(response => response.json())
@@ -12,14 +13,17 @@ export default function MyProfile() {
         fetch('http://localhost:8080/api/user/' + jsonData.ID, { mode: 'cors', credentials: 'include' })
           .then(response => response.json())
           .then((jsonData) => {
-            data = JSON.parse(jsonData)
-            console.log(jsonData)
+            var datum = new Date(Date.parse(jsonData.datum_rodjenja))
+            jsonData.datum_rodjenja = datum.toISOString().split('T')[0]
+            setPodaci(jsonData)
           })
           .catch((error) => {
+            setPodaci(myError);
             console.error(error)
           })
       })
       .catch((error) => {
+        setPodaci(myError);
         console.error(error)
       })
   }, []);
@@ -48,22 +52,20 @@ export default function MyProfile() {
             <hr className="myLine" />
 
             <label><b className="bold">Email</b></label>
-            <input type="email" className="myimput" />
+            <input type="email" className="myimput" value={podaci.email} readOnly={true} />
 
             <label><b className="bold">Ime</b></label>
-            <input type="text" className="myimput" />
+            <input type="text" className="myimput" value={podaci.ime} readOnly={true} />
 
             <label><b className="bold">Prezime</b></label>
-            <input type="text" className="myimput" />
+            <input type="text" className="myimput" value={podaci.prezime} readOnly={true} />
 
             <label><b className="bold">Username</b></label>
-            <input type="text" className="myimput" />
+            <input type="text" className="myimput" value={podaci.username} readOnly={true} />
 
             <label><b className="bold">Datum rodjenja</b></label>
-            <input type="date" className="myimput" />
+            <input type="date" className="myimput" value={podaci.datum_rodjenja} readOnly={true} />
             <hr className="myLine" />
-
-            <button type="submit" class="registerbtn">Register</button>
           </form>
         </div>
         <div className="right-sidebar"></div>
