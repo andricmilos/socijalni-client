c7f694942079cebd64d5adb5e4abcfa45eef977e
Milos Andric <milosandric2000@gmail.com>
2023-02-14 14:14:10 +0100

    Dodata provera za vrstu naloga

--- src/LoginValidate.js
+++ src/LoginValidate.js
@@ -1,14 +1,18 @@
 export default function validateLogin() {
-    var url = 'http://localhost:8080/api/validate/login'
-    fetch(url, { mode: 'cors', credentials: 'include' })
-      .then((response) => {
-        if (response.url != url) {
-          window.location.href = "/login";
-        }
-      })
-      .catch((error) => {
-        console.error(error)
-      });
-  
+  var url = 'http://localhost:8080/api/validate/login'
+  fetch(url, { mode: 'cors', credentials: 'include' })
+    .then((response) => {
+      if (response.url != url) {
+        window.location.href = "/login";
+      }
+      return response.text()
+    }).then((text)=>{
+      if(text != "basic"){
+        window.location.href = "/login";
+      }
+    })
+    .catch((error) => {
+      console.error(error)
+    });
+
 }
-  
